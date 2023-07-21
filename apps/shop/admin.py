cbb1a13d4ba11525c70d0a1f216bd8e356b7a3c8
Prashanth <prashanth.cheepelli@yahoo.com>
2023-07-21 22:55:29 +0400

    nav bar

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
 from .models import Category, Order, OrderItem, Product, ShippingAddress, Tag
 
-admin.site.register(Product)
-admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
-# admin.site.register(Customer)
+
+
+@admin.register(Category)
+class CategoryAdmin(MPTTModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
--- apps/shop/models.py
+++ apps/shop/models.py
@@ -8,8 +8,6 @@ from mptt.models import MPTTModel, TreeForeignKey
 from apps.common.models import TimeStampedUUIDModel
 from apps.users.models import User
 
-# from django.contrib.auth.models import User
-
 
 class Tag(TimeStampedUUIDModel):
     name = models.CharField(max_length=200)
@@ -74,15 +72,6 @@ class Product(TimeStampedUUIDModel):
         except:
             url = ""
         return url
-
-
-# class Customer(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-#     name = models.CharField(max_length=200, null=True, blank=True)
-#     email = models.EmailField(max_length=200, null=True, blank=True)
-
-#     def __str__(self):
-#         return self.name
 
 
 class Order(TimeStampedUUIDModel):
